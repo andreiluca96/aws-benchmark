9216416ed2237367465a7314ab0c117b2348f6c6
Andrei Luca <luca.andrei96@gmail.com>
2024-05-25 10:10:27 +0300

    fix basic infra

--- infra/lib/constructs/benchmarking-resources.ts
+++ infra/lib/constructs/benchmarking-resources.ts
@@ -1,7 +1,7 @@
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as sns from 'aws-cdk-lib/aws-sns';
-
+import * as events from 'aws-cdk-lib/aws-events'
 import { Duration } from 'aws-cdk-lib';
 
 export interface BenchmarkingResourcesProps {
@@ -12,12 +12,16 @@ export class BenchmarkingResources extends Construct {
   constructor(scope: Construct, id: string, props: BenchmarkingResourcesProps) {
     super(scope, id);
 		
-		const queue = new sqs.Queue(this, 'InfraQueue', {
+		const queue = new sqs.Queue(this, 'BenchmarkQueue', {
       visibilityTimeout: Duration.seconds(300),
 			queueName: `${props.prefix}-benchmark-queue`
     });
 
-    const topic = new sns.Topic(this, 'InfraTopic', {
+		const event_bus = new events.EventBus(this, 'BenchMarkEventBus', {
+			eventBusName: `${props.prefix}-benchmark-event-bus`
+		});
+
+    const topic = new sns.Topic(this, 'BenchmarkTopic', {
 			topicName: `${props.prefix}-benchmark-topic`
 		});
   }
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -3,17 +3,14 @@ import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import { BenchmarkingResources } from './constructs/benchmarking-resources'
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'InfraQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'InfraTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+		new BenchmarkingResources(this, 'BenchmarkingResources', {
+			 prefix: 'eu-central-1'
+		})
   }
 }
